1dd5caaec269c7b05779898c1f0d1e403e9277f2
elkinssm <elkinsilvamanrique@gmail.com>
2023-06-06 18:23:29 -0500

    Add menu popup

--- main/ui/pages/home/home.js
+++ main/ui/pages/home/home.js
@@ -7,71 +7,106 @@ Page({
     });
   },
   data: {
+    basicVisible: true,
+    position: "top",
     access: [
       {
-        icon: "/main/ui/assets/icons/consumo.svg",
-        text: "Consulta\n consumos",
+        icon: "/main/ui/assets/icons/moviles.svg",
+        text: "Soluciones \nmóviles",
         appId: "3482020158910265"
       },
       {
-        icon: "/main/ui/assets/icons/detalle.svg",
-        text: "Detalle de\n tu plan",
+        icon: "/main/ui/assets/icons/banda-ancha.svg",
+        text: "Red banda \nancha (HFC)",
         appId: ""
       },
       {
-        icon: "/main/ui/assets/icons/paquetes.svg",
-        text: "Compra de paquetes",
+        icon: "/main/ui/assets/icons/fibra-optica.svg",
+        text: "Red de\nfibra óptica",
         appId: ""
       },
       {
-        icon: "/main/ui/assets/icons/portafolio.svg",
-        text: "Portafolio de soluciones",
+        icon: "/main/ui/assets/icons/historial.svg",
+        text: "Historial\nde facturas",
         appId: ""
       },
       {
         icon: "/main/ui/assets/icons/pagos.svg",
-        text: "Pagos\n en línea",
-        appId: ""
-      },
-      {
-        icon: "/main/ui/assets/icons/tramites.svg",
-        text: "Información de tramites",
-        appId: ""
-      },
-      {
-        icon: "/main/ui/assets/icons/chat.svg",
-        text: "Chat empresarial",
-        appId: ""
-      },
-      {
-        icon: "/main/ui/assets/icons/guia.svg",
-        text: "Guía y novedades",
+        text: "Administrar\npagos",
         appId: ""
       },
       {
         icon: "/main/ui/assets/icons/equipos.svg",
-        text: "Devolución\n de equipos",
+        text: "Equipos\nfinanciados",
         appId: ""
+      }
+    ],
+    menuAccess: [
+      {
+        iconAccess: "/main/ui/assets/icons/users.svg",
+        titleAccess: "Administrar perfiles",
+        pageUrl: "/pages/test/test"
+      },
+      {
+        iconAccess: "/main/ui/assets/icons/edit.svg",
+        titleAccess: "Personalizar servicios",
+        pageUrl: "/pages/test/test"
+      },
+      {
+        iconAccess: "/main/ui/assets/icons/user.svg",
+        titleAccess: "Gestion de la cuenta",
+        pageUrl: "/pages/test/test"
+      }
+    ],
+    footerItems: [
+      {
+        icon: "/main/ui/assets/icons/inicio.svg",
+        title: "Inicio",
+        pageUrl: "/pages/test/test"
+      },
+      {
+        icon: "/main/ui/assets/icons/adquirir.svg",
+        title: "Adquirir",
+        pageUrl: "/pages/test/test"
+      },
+      {
+        icon: "/main/ui/assets/icons/chat.svg",
+        title: "Chat",
+        pageUrl: "/pages/test/test"
+      },
+      {
+        icon: "/main/ui/assets/icons/pedidos.svg",
+        title: "Pedidos",
+        pageUrl: "/pages/test/test"
       }
     ]
   },
-navigateToMiniProgram(e) {
-  const appId = e.target.dataset.appId;
-  const index = e.target.dataset.index;
-  console.log(`Navigating to mini program with appId: ${appId} at index: ${index}`);
-  my.navigateToMiniProgram({
-    appId: appId,
-    path: "pages/home/home",
-    extraData: {
-      data1: "test"
-    },
-    success(res) {
-      console.log("Navigated to mini program successfully", res);
-    },
-    fail(err) {
-      console.error("Failed to navigate to mini program", err);
-    }
-  });
-},
+  navigateToMiniProgram(e) {
+    const appId = e.target.dataset.appId;
+    const index = e.target.dataset.index;
+    console.log(
+      `Navigating to mini program with appId: ${appId} at index: ${index}`
+    );
+    my.navigateToMiniProgram({
+      appId: appId,
+      path: "pages/home/home",
+      extraData: {
+        data1: "test"
+      },
+      success(res) {
+        console.log("Navigated to mini program successfully", res);
+      },
+      fail(err) {
+        console.error("Failed to navigate to mini program", err);
+      }
+    });
+  },
+  handleShowMenu(e) {
+    const { position } = e.target.dataset;
+    this.setData({
+      position,
+      basicVisible: true
+    });
+  },
   onUnload() {}
 });
