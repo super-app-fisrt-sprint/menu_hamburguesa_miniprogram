5fd05c40f46ed9a7c0c62bb1f2988bc279b07181
Ronal Gelvez <rmgelvez@hotmail.com>
2023-09-13 12:19:20 -0500

    Update DeviceSpectViewModel.js

--- main/domain/DeviceSpectViewModel.js
+++ main/domain/DeviceSpectViewModel.js
@@ -23,21 +23,29 @@ function CreateInfoDeviceStorage (data) {
     const tokenRepository = new AutoRefreshRepository();
 
     if (data) {
-      const extraData = data.response[1].data.response
+      const extraData = data.response[1].data.response;
 
-      // LoginInfo
-      const usuario = extraData.usuario;
-      tokenRepository.changeInfoLoginLocal("nombre", usuario.nombre);
-      tokenRepository.changeInfoLoginLocal("email", usuario.UserProfileID);
-      tokenRepository.changeInfoLoginLocal("apellido", usuario.apellido);
-      tokenRepository.changeInfoLoginLocal("DocumentNumber", usuario.DocumentNumber);
-      loginInfoUpdated = tokenRepository.changeInfoLoginLocal("DocumentType", usuario.DocumentType);
+      const infoLogin = {
+        nombre: extraData.usuario.nombre,
+        email: extraData.usuario.UserProfileID,
+        apellido: extraData.usuario.apellido,
+        DocumentNumber: extraData.usuario.DocumentNumber,
+        DocumentType: extraData.usuario.DocumentType
+      };
 
-      // DevicesSpect
-      tokenRepository.changeHeaderLocal("X-SESSION-ID", extraData.cuentas[0].token);
-      tokenRepository.changeHeaderLocal("X-MC-MAIL", extraData.usuario.UserProfileID);
-      tokenRepository.changeHeaderLocal("X-MC-USER-AGENT", "eyJpcCI6IjE3Mi4yMS44My4yNSIsInVzZXJBZ2VudCI6Ik1pQ2xhcm9BcHAvMC4wLjEgKHNhbXN1bmc7IFNNLUc5ODhOOyBcdTAwM2NhbmRyb2lkLzlcdTAwM2UpIn0=");
-      headersUpdated = tokenRepository.changeHeaderLocal("X-MC-DEVICE-ID", "sdSS5V/uzNMk7u+w5J7jrLK82uPH+QeVMag0lGPgjM/XZ5KOc7MKnHFsFNb8kPQtil3fH8ewmXvXD88huw4LGecaguoggK6aWSq+o3TmC0uyWNagvvJpl2R8VGwFdiil/JDQXF/JXv5Jm8nA+lr0TEMDDWqfc5bEtnOvX9mmmaQ=");
+      const deviceSpect = {
+        "X-SESSION-ID": extraData.cuentas[0].token,
+        "X-MC-MAIL": extraData.usuario.UserProfileID,
+        "X-MC-USER-AGENT": "eyJpcCI6IjE3Mi4yMS44My4yNSIsInVzZXJBZ2VudCI6Ik1pQ2xhcm9BcHAvMC4wLjEgKHNhbXN1bmc7IFNNLUc5ODhOOyBcdTAwM2NhbmRyb2lkLzlcdTAwM2UpIn0=",
+        "X-MC-DEVICE-ID": "sdSS5V/uzNMk7u+w5J7jrLK82uPH+QeVMag0lGPgjM/XZ5KOc7MKnHFsFNb8kPQtil3fH8ewmXvXD88huw4LGecaguoggK6aWSq+o3TmC0uyWNagvvJpl2R8VGwFdiil/JDQXF/JXv5Jm8nA+lr0TEMDDWqfc5bEtnOvX9mmmaQ="
+      };
+
+      for (const key in infoLogin) {
+        loginInfoUpdated = tokenRepository.changeInfoLoginLocal(key, infoLogin[key]);
+      }
+      for (const key in deviceSpect) {
+        headersUpdated = tokenRepository.changeHeaderLocal(key, deviceSpect[key]);
+      }
 
       result.success = true;
       result.data = {
