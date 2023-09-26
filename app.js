f0fd9d2ae3e2d7dedc5c9a9db2b852d6a6a789a5
Ronal Gelvez <rmgelvez@hotmail.com>
2023-09-25 21:14:32 -0500

    Tests

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const DeviceSpectModel = require("/main/domain/DeviceSpectViewModel");
 App({
-  onLaunch () {
+  onLaunch (options) {
+    
     const { referrerInfo: { extraData } = {} } = my.getLaunchOptionsSync();
+
+    my.clearStorageSync();
+    my.alert({content: {extraData: extraData}});
+
     const deviceData = my.getSystemInfoSync();
     DeviceSpectModel.CreateInfoDeviceStorage(extraData, deviceData);
+    
+    
     my.setStorageSync({ key: 'extraData', data: extraData });
   }
 });
